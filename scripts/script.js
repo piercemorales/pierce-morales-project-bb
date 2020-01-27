5c3463959d7ab6d4aa1c2608aed7f01bdd305041
Pierce <nathanm0220@gmail.com>
2020-01-27 16:59:00 -0500

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// A mock (nike) e-commerce site where users are able to look through products and add items to their "cart"
+// user is able to view their cart and see items they have added
+//users have the option to remove items from cart (empty cart)
+// sticky nav with the cart menu slides out to half or full page
+//cart calculate the value of items total price 
+
+
+
+//stretch goals 
+// user is able to browse various categories
+//add filter/sort feature that organizes alphabetically and price: low to high, high to low
+//photos of products rotate on hover
+//display different colorways of certain shoes
+//display video of shoe 
